15b5389ec35b59b777415bf33182390e7fa33eb8
GhaniC <abdelghanicherrak@oulook.fr>
2025-03-27 11:55:28 +0100

    fct vide

---
+++ DataSetCreator/__init__.py
@@ -0,0 +1,7 @@
+def choix_dataset(l_maj,l_min,chiffres,ponctu_simples,ponctu_complex) :
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
+
+    return (X_train,Y_train,X_test,Y_test)
