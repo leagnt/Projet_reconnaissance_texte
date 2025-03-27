e39a30a36c1153f47a013f89d715658cbe7098f1
Clement <aaazert7@gmail.com>
2025-03-27 11:53:11 +0100

    ojàa

--- nettoyage.py
+++ nettoyage.py
@@ -19,14 +19,6 @@ class image():
         return image_array
 
 
-    def enlever_ombre(self,image,imagepath):
-        #prend en paramètres une matrice pour image et renvoie une matrice
-        #Valeurs entre 0 et 255
-        enhancer = ImageEnhance.Brightness(image)
-        image_eclairee = enhancer.enhance(1.5)
-        image_eclairee.show()
-        image_eclairee.save(imagepath)
-
     def griser_image(self,imagepath):
         image = Image.open(imagepath)
         image_grise = image.convert("L")
