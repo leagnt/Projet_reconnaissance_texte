f57d2b42f2b827ad62d6724383b63ef91c785164
Mathisrrr <mathisrivallan5@gmail.com>
2025-03-27 11:16:40 +0100

    new

--- nettoyage.py
+++ nettoyage.py
@@ -1,12 +1,21 @@
+import random
+from PIL import Image
+import numpy as np
 
-from PIL import Image, ImageEnhance
 class image():
 
     def __init__(self):
         return None
 
-    def ouvrir_fenetre(self):
-        return None
+    def ouvrir_images(self):#Renvoie la liste d'une image
+        image = Image.open("./images/test.png")
+        image = image.resize((1000, 1000))
+        image = image.convert("L")  # Convertir en niveaux de gris
+        image_array = np.asarray(image).flatten()
+        image.show()
+        image_array=255-image_array
+        return image_array
+
 
     def enlever_ombre(self,image,imagepath):
         enhancer = ImageEnhance.Brightness(image)
@@ -17,3 +26,8 @@ class image():
         image = Image.open(imagepath)
         image_grise = image.convert("L")
         pixels = list(image_grise.getdata())
+
+A=image()
+test=A.ouvrir_images()
+
+print(test)
