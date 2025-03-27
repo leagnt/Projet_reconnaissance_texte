2b76f78a72d8a39889a41f7aa493ddd49d1b9fc7
Mathisrrr <mathisrivallan5@gmail.com>
2025-03-27 11:31:11 +0100

    new

--- nettoyage.py
+++ nettoyage.py
@@ -12,8 +12,8 @@ class image():
         image = image.resize((1000, 1000))
         image = image.convert("L")  # Convertir en niveaux de gris
         image_array = np.asarray(image).flatten()
-        image.show()
-        image_array=255-image_array
+        image_array=255-image_array #O pour le blanc et 255 pour le noir
+
         image_array=image_array.reshape(1000,1000)
         print(image_array.shape)
         return image_array
