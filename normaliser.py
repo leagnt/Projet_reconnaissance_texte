f883f0b95511b6325c1e80444ec3daed0f0643b9
leagnt <leagoyhenetche@gmail.com>
2025-03-27 11:56:21 +0100

    Merge branch 'main' of /Users/leagnt/Desktop/GitHub/Projet_reconnaissance_texte with conflicts.

--- normaliser.py
+++ normaliser.py
@@ -1,8 +1,19 @@
 import numpy as np
-import pandas as pd
 import cv2
-class normaliser:
-    def __init__(self, image_lettre, dim=(28, 28)):
-        image_normal = image_lettre.np.reshape(dim)
 
-        return image_normal
+class Normaliser:
+    def __init__(self, images_lettres, dim=(28, 28)):
+        # Liste pour stocker les images normalisées
+        self.images_normal = []
+
+        for image_lettre in images_lettres:
+            # Redimensionner (28x28)
+            image_normal = cv2.resize(image_lettre, dim)
+            # Normaliser
+            image_normal = image_normal / 255.0
+            # Aplatir l'image
+            image_normal = image_normal.reshape(-1)  # 784 éléments pour une image 28x28
+            self.images_normal.append(image_normal)
+
+    def get_images(self):
+        return np.array(self.images_normal)
