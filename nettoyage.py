d47da4930d3ebdd99adc2b9a4ed22d5d6358866d
Clement <aaazert7@gmail.com>
2025-03-27 12:15:52 +0100

    modif

--- nettoyage.py
+++ nettoyage.py
@@ -73,14 +73,6 @@ class image():
 
         return matrice_image
 
-
-
-
-
-
-
-
-
     def rogner_image(self):
         lignes = len(self.matrice)
         colonnes = len(self.matrice[0]) if lignes > 0 else 0
@@ -128,15 +120,6 @@ class image():
         if bas < haut or droite < gauche:
             return []  # Retourne une image vide si aucun pixel trouvé
 
-    def enlever_ombre(self,image,imagepath):
-        enhancer = ImageEnhance.Brightness(image)
-        image_eclairee = enhancer.enhance(1.5)
-        image_eclairee.show()
-        image_eclairee.save(imagepath)
-    def griser_image(self,imagepath):
-        image = Image.open(imagepath)
-        image_grise = image.convert("L")
-        pixels = list(image_grise.getdata())
 
 A=image()
 test=A.ouvrir_images()
