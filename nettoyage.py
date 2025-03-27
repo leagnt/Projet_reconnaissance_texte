2d43bfe17603f363c6931cf683f45a1e1842d1fa
clement-dotcom <clemdechaux16@gmail.com>
2025-03-27 10:50:28 +0100

    mathis

--- nettoyage.py
+++ nettoyage.py
@@ -1,5 +1,5 @@
-import random
 
+from PIL import Image, ImageEnhance
 class image():
 
     def __init__(self):
@@ -7,3 +7,13 @@ class image():
 
     def ouvrir_fenetre(self):
         return None
+
+    def enlever_ombre(self,image,imagepath):
+        enhancer = ImageEnhance.Brightness(image)
+        image_eclairee = enhancer.enhance(1.5)
+        image_eclairee.show()
+        image_eclairee.save(imagepath)
+    def griser_image(self,imagepath):
+        image = Image.open(imagepath)
+        image_grise = image.convert("L")
+        pixels = list(image_grise.getdata())
