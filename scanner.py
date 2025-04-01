f36f8a8e383223d4a399116e28e719a016205dc9
annalebrun <anna.lebrun.jd@gmail.com>
2025-04-01 11:48:03 +0200

    j
    iug

--- scanner.py
+++ scanner.py
@@ -1,9 +1,6 @@
-
 class scanner:
-
-    def __init__(self,base):
-
-        self.base = base
+    def __init__(self):
+        self.base = [[1,1,1],[1,1,0],[1,0,1],[1,1,0],[1,1,1],[0,0,0]]
         self.liste_hauteur = []
         self.matrice_image = [[]]
 
@@ -22,31 +19,52 @@ class scanner:
             self.liste_hauteur.append(taille_ligne)
             condition = self.inverse(condition)
 
+
         print(self.liste_hauteur)
 
 
     def trouve_lettre(self):
+
+        indice_ligne = 0
+        indice_taille_ligne = 1
+        taille_ligne = self.liste_hauteur[indice_taille_ligne]
+        print(taille_ligne)
+
+
         i = 0
         condition = True
-        while i < len(self.base):
+        print(len(self.base[0]),"f")
+        while i < len(self.base[0]):
 
-            longueur_lettre = 0
 
-            while i < len(self.base) and self.ligne_blanche(i) is condition:
+            indice = i
+            print(i)
+
+            while i < len(self.base[0]) and self.colonne_blanche(i,indice_ligne,taille_ligne) is condition:
+                print( self.colonne_blanche(i,indice_ligne,taille_ligne),"jfjjff")
                 i += 1
-                longueur_lettre += 1
 
-            #self.liste_hauteur.append(self.base[])
+
+
+            sub_matrix = [row[indice:i] for row in self.base[indice_ligne:(indice_ligne+taille_ligne)]]
+
+            print(sub_matrix ,"hjrfjf",indice, i )
+
+            self.matrice_image.append(sub_matrix )
+
             condition = self.inverse(condition)
 
-        print(self.liste_hauteur)
+        print(self.matrice_image)
 
-    #sous_matrice = [row[20:30] for row in matrice[10:20]]
+
 
     def inverse(self, condition):
         if condition is True:
             return False
         return True
+
+
+
 
 
     def ligne_blanche(self, indice):
@@ -60,12 +78,11 @@ class scanner:
     def colonne_blanche(self, indice_colonne, indice_ligne, taille):
         # pour un indice de colonne, renvoie vrai si toutes les pixels de cette colonne sont blancs, faux sinon
         for i in range(taille):
-             if self.base[indice_ligne + i ][indice_colonne] == 0:
-                 return False
-        return True
+             if self.base[indice_ligne + (i) ][indice_colonne] == 0:
+                 return True
+        return False
 
-test = [[1,1,1],[1,1,1],[0,1,0],[1,0,1],[1,1,1],[0,0,0]
-                     ]
 
-scanner = scanner(test)
+scanner = scanner()
 scanner.hauteur_ligne()
+scanner.trouve_lettre()
