d47b24c684b62385bb275a4f7877bf30ad6135e1
David29483 <d.ferreiradocarmo49@gmail.com>
2025-04-01 16:46:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from nettoyage import image
+from nettoyage2 import afficher_matrice
 from scanner import scanner
+from normaliser import Normaliser
 import numpy as np
+from nettoyage2 import afficher_matrice
+import matplotlib
 path="images/test.png"
 
 im=image()
@@ -16,4 +20,17 @@ pic_rogner = pic_rogner.tolist()
 
 scanner = scanner(pic_rogner)
 Images = scanner.scanner()
-print(Images)
+np_Images = [np.array(sublist) for sublist in Images]
+
+Normaliser = Normaliser(np_Images)
+Images_finales = Normaliser.renvoyer_image()
+
+print(Images_finales)
+for Images in Images_finales :
+    print(Images.shape)
+    print("okkk")
+    I=Images.reshape(28,28)
+    print(I.shape)
+    afficher_matrice(I)
+
+
