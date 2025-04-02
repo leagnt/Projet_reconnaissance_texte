fc4790caa0059acca5093e50f10618e1b03cc928
adrienlanoe5 <adrienlanoe5@gmail.com>
2025-04-02 20:21:17 +0200

    nettoyage ombre

--- nettoyage2.py
+++ nettoyage2.py
@@ -28,7 +28,7 @@ def ouvrir_images(path):#Chemin de l'image en entrée,renvoie une liste 2 dim de
     return image_array
 
 
-def enlever_ombre_deux(self, matrice_image, offset):
+def enlever_ombre_deux( matrice_image, offset=0):
     ## prend en argument l'image sous la forme d'une matrice numpy et revoie une image sous la forme d'une matrice numpy de pixels noirs ou blancs
 
     # application du filtre
@@ -45,25 +45,27 @@ def enlever_ombre_deux(self, matrice_image, offset):
     return matrice_image
 
 
-def ombres_3(matrice_image, finesse=20):
+def ombres_3(matrice_image, finesse=100):
+    matrice_image=matrice_image/255
     liste = [0 for i in range(finesse)]
     precision = 1 / finesse
     dim_mat = np.shape(matrice_image)
     for ligne in range(dim_mat[0]):
         for colonne in range(dim_mat[1]):
-            valeur_pixel = matrice_image[ligne, colonne] / 255
+            valeur_pixel = matrice_image[ligne, colonne]
             rang = int(float(valeur_pixel) // precision)
             if valeur_pixel==1:
                 rang=finesse-1
             liste[rang] += 1
 
     minimums = []
+
+
     for i in range(1, len(liste) - 1):
         if liste[i] < liste[i + 1] and liste[i] > liste[i - 1]:
             minimums.append(i)
 
-    seuil = minimums[0] * precision
-    print()
+    seuil = minimums[-1] * precision
 
     for ligne in range(dim_mat[0]):
         for colonne in range(dim_mat[1]):
@@ -74,8 +76,9 @@ def ombres_3(matrice_image, finesse=20):
     return matrice_image
 
 im=ouvrir_images("images/test.png")
-print(im)
-im2=ombres_3(im, 30)
+
+im2=ombres_3(im, 100)
+im3=enlever_ombre_deux(im)
 
 afficher_matrice(im2)
 #ok
